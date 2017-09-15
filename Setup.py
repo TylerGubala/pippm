621dc38bdb197be8fdbd7c2b7642af15112c8d7e
Austin Gubala <austingubala@gmail.com>
2017-09-14 20:54:47 -0400

    Created Setup.py, initialization script created.

---
+++ Setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pippm',
+      version='0.1',
+      description='A package manager for Python',
+      
+      author='Tyler Gubala',
+      author_email='gubalatyler@yahoo.com',
+      license='GPL-3.0',
+      packages=['pippm'],
+      zip_safe=False)
