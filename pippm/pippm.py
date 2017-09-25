4c3d98bf13dd8aded2b911018524dc69c1bc5991
T Gubs <T Gubs>
2017-09-24 20:55:09 -0400

    Added better options for argument parsing

--- pippm/pippm.py
+++ pippm/pippm.py
@@ -1,28 +1,77 @@
 def main():
     try:
 
-        import argparse, subprocess
+        import argparse
 
-    except Exception as ex_inst:
+    except ImportError as error:
 
-        print("Exception occured, details: ".format(ex_inst))
+        print("pippm failed, could not import required modules.\nError: {0}".format(error))
 
     else:
+
+        parser = argparse.ArgumentParser(
+            description="""A package manager tool for python.\n
+            Includes:\n
+            \t-automatic virtual environment creation\n
+            \t-automatic tracking of packages as they are installed\n
+            \t-helpers for pip (creation of .pypirc and addition of .pypirc to .gitignore)""",
+            formatter_class=argparse.RawTextHelpFormatter
+            )
+
+        parser.add_argument("name", help="The name of the package")
         
-        parser = argparse.ArgumentParser()
-        
-        parser.add_argument("name", help="The package that you want to create")
-        
+        group = parser.add_mutually_exclusive_group()
+
+        group.add_argument("--init", "-n", help="Create the virtual environment named after the package", action="store_true")
+
+        group.add_argument("--remove", "-r", help="Remove package and associated virtual environment", action="store_true")
+
+        group.add_argument("--install", "-i", help="Using pip, install a dependency at the named package", action="store_true")
+
+        group.add_argument("--uninstall", "-u", help="Using pip, uninstall a dependency at the named package", action="store_true")
+
+        parser.add_argument("--dependency", help="The name of the dependency to install/uninstall", required=parser.parse_args().uninstall or parser.parse_args().install)
+
         args=parser.parse_args()
-        
-        print("Are you sure you want to create this package? ({0})".format(args.name)
-        
-        if input("Y for yes, N for no.") is "Y":
+
+        if args.init:
+
+            initialize(args)
+
+def initialize(args):
+
+    try:
+
+        import asyncio, json, os, venv
+
+    except ImportError as error:
+
+        print("Initialization failed, could not import required packages. \n Error: {0}".format(error))
+
+    else:
+
+        print("Are you sure you want to create this package? ({0})".format(args.name))
+
+        if input("Y for yes, N for no.").upper() is "Y" or "YES":
             
-            print("Creating package... {0}".format(args.name)
+            print("Creating package... {0}".format(args.name))
             
         else:
             
             print("User canceled package creation. Package {0} was not created.".format(args.name))
+
+    #Initialization code here
+
+def install():
+    pass
+    #install code here
+
+def uninstall():
+    pass
+    #uninstall code here
+
+def test():
+    pass
+    #test code here
 
 #subprocess.call("py -m venv {0}".format())
